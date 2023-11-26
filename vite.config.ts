91fdb3074f6bcb011579641653977ebfc22fff13
Your Name <you@example.com>
2023-11-26 20:01:00 +0100

    deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-      watch: {
-        usePolling: true,
-      },
-      host: true, // needed for the Docker Container port mapping to work
-      strictPort: true,
-      port: 5173 // you can replace this port with any port
+  server: {
+    watch: {
+      usePolling: true,
     },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173 // you can replace this port with any port
+  },
+  base: '/good-news'
 })
